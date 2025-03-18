d093036deedc204215b8a09d88b7e04f46ac5d06
ValeryLangolf <lvv.89@rambler.ru>
2025-03-18 03:37:13 +0300

    Добавлены звуки улицы, приглушение их в доме и увеличение в лесу. Поставил ночной скайбокс

---
+++ Assets/Scripts/Audio/ForestSoundVolumeChanger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ForestSoundVolumeChanger : MonoBehaviour
+{
+    [SerializeField] private AudioSource _source;
+    [SerializeField, Range(0, 1)] private float _targetVolume;
+    [SerializeField] private float _speed;
+    private Coroutine _coroutine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeVolumeOverTime());
+    }
+
+    private IEnumerator ChangeVolumeOverTime()
+    {
+        while (_source.volume != _targetVolume)
+        {
+            _source.volume = Mathf.MoveTowards(_source.volume, _targetVolume, _speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+}
